26e9ce3864d6848d55c2329102f910bfbdb99f98
Shubham Shrestha <shubhamshrestha25@gmail.com>
2021-05-18 10:04:23 +0545

    adding themeContext

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Todolist from "./components/Todolist";
+import ThemeContextProvider from "./context/ThemeContext";
 
 function App() {
   return (
     <div className="App">
-      <div className='ui raised very padded text container segment'>
-        <Navbar />
-        <Todolist />
+      <div className="ui raised very padded text container segment">
+        <ThemeContextProvider>
+          <Navbar />
+          <Todolist />
+        </ThemeContextProvider>
       </div>
     </div>
   );
