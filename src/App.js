fc6b8d2d730189d5641a9653d75598979278597b
Shubham Shrestha <shubhamshrestha25@gmail.com>
2021-05-21 20:12:00 +0545

    adding auth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Todolist from "./components/Todolist";
+import AuthContextProvider from "./context/AuthContext";
 import ThemeContextProvider from "./context/ThemeContext";
 
 function App() {
   return (
     <div className="App">
       <div className="ui raised very padded text container segment">
-        <ThemeContextProvider>
-          <Navbar />
-          <Todolist />
-        </ThemeContextProvider>
+        <AuthContextProvider>
+          <ThemeContextProvider>
+            <Navbar />
+            <Todolist />
+          </ThemeContextProvider>
+        </AuthContextProvider>
       </div>
     </div>
   );
